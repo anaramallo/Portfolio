2502c1e2cb679a5600dcc7df2d1ec792991be97a
Ana <anaramallogonzalez@gmail.com>
2025-10-11 15:44:13 +0200

    update: migrar API de Render a Railway

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_BASE_URL: 'https://portfolio-api-dvx9.onrender.com', // url render
+  API_BASE_URL: 'https://portfolio-production-f158.up.railway.app',
   demoUrls: {
     portfolio: 'https://www.anaramallo.dev'
   }
